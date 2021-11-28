dbceeda8925bdf2e21344bb52158ab84bfcffb41
Ramasubramanya-MS <81099796+Ramasubramanya-MS@users.noreply.github.com>
2021-11-28 17:49:21 +0530

    push

---
+++ app/models/task.rb
@@ -0,0 +1,21 @@
+class Task < ApplicationRecord
+  belongs_to :project
+
+  validates :status, inclusion: { in: ['not-started', 'in-Progress', 'complete']}
+
+  STATUS_OPTIONS = [
+    ['Not started','not-started'],
+    ['In Progress','in-progress'],
+    ['Complete','complete']]
+
+  def badge_color
+    case status
+    when 'not started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+end
